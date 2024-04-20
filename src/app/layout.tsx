2893eee9ea853a451e967f8034cdf5feb5d00091
hs <tlsgustn93@naver.com>
2024-04-21 03:34:04 +0900

    feat: Metadata 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "TOTAL SEARCH",
-  description: "totalSearch",
+  description: "중고 거래 사이트를 한번에 검색하는 토탈 서치",
   icons: {
     icon: "/yellowstar.png",
   },
