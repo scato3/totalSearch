622961637ef377be5d673e263bec052f468566ff
hs <tlsgustn93@naver.com>
2024-04-21 03:37:19 +0900

    fix: 자잘한 css 수정 및 필요없는 파일 삭제

--- src/app/_component/Navbar.tsx
+++ src/app/_component/Navbar.tsx
@@ -1,5 +1,5 @@
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
-  return <div className={styles.navContainer}>통합 검색 사이트</div>;
+  return <div className={styles.navContainer}>T O T A L S E A R C H</div>;
 }
