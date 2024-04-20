b001e90e5bdc96c14175d619bea5751e4ede06e3
hs <tlsgustn93@naver.com>
2024-04-21 03:31:42 +0900

    fix: favicon 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "TOTAL SEARCH",
   description: "totalSearch",
+  icons: {
+    icon: "/yellowstar.png",
+  },
 };
 
 export default function RootLayout({
